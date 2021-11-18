f9a6addf8cc234ca5865bc8bc83aa8221c8abd23
haozhicao <helloted@live.com>
2021-11-19 00:06:41 +0800

    Update install.sh
    ssl

--- install.sh
+++ install.sh
@@ -624,7 +624,7 @@ nginx_process_disabled() {
 #    judge "rc.local 配置"
 #}
 acme_cron_update() {
-    wget -N -P /usr/bin --no-check-certificate "https://raw.githubusercontent.com/helloted/world/dev/ssl_update.sh"
+    wget -N -P /usr/bin --no-check-certificate "https://raw.githubusercontent.com/helloted/world/${github_branch}/ssl_update.sh"
     if [[ $(crontab -l | grep -c "ssl_update.sh") -lt 1 ]]; then
       if [[ "${ID}" == "centos" ]]; then
           #        sed -i "/acme.sh/c 0 3 * * 0 \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
